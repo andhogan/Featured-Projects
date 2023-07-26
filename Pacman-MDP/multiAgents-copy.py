de88756a1ebe648819587d2bc7c01a741595eff2
andhogan <andrew.hogan4.0@gmail.com>
2023-07-26 03:59:34 -0400

    Uploaded Pacman-MDP project

---
+++ Pacman-MDP/multiAgents-copy.py
@@ -0,0 +1,79 @@
+class ReflexAgent(Agent):
+    """
+      A reflex agent chooses an action at each choice point by examining
+      its alternatives via a state evaluation function.
+
+      The code below is provided as a guide.  You are welcome to change
+      it in any way you see fit, so long as you don't touch our method
+      headers.
+    """
+
+
+    def getAction(self, gameState):
+        """
+        You do not need to change this method, but you're welcome to.
+
+        getAction chooses among the best options according to the evaluation function.
+
+        Just like in the previous project, getAction takes a GameState and returns
+        some Directions.X for some X in the set {North, South, West, East, Stop}
+        """
+        # Collect legal moves and successor states
+        legalMoves = gameState.getLegalActions()
+
+        # Choose one of the best actions
+        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+
+        "Add more of your code here if you want to"
+
+        return legalMoves[chosenIndex]
+
+    def evaluationFunction(self, currentGameState, action):
+        """
+        Design a better evaluation function here.
+
+        The evaluation function takes in the current and proposed successor
+        GameStates (pacman.py) and returns a number, where higher numbers are better.
+
+        The code below extracts some useful information from the state, like the
+        remaining food (newFood) and Pacman position after moving (newPos).
+        newScaredTimes holds the number of moves that each ghost will remain
+        scared because of Pacman having eaten a power pellet.
+
+        Print out these variables to see what you're getting, then combine them
+        to create a masterful evaluation function.
+        """
+        # Useful information you can extract from a GameState (pacman.py)
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+        newPos = successorGameState.getPacmanPosition()
+        newFood = successorGameState.getFood()
+        newGhostStates = successorGameState.getGhostStates()
+        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+
+        "*** YOUR CODE HERE ***"
+        # Get PacMan's current pos
+        currentPos = list(successorGameState.getPacmanPosition())
+        
+        # List remaining food
+        food = currentGameState.getFood().asList()
+        
+        # Pacman does not need to move
+        if action == 'STOP':
+            return -inf
+          
+        # If Ghost would move to PacMan's position, do not move to this space.
+        for state in newGhostStates:
+            if state.getPosition() == tuple(currentPos) and (state.scaredTimer == 0):
+                return -inf
+          
+        # Calculate distance to closest food
+        distance = -inf
+        for x in food:
+            tempDistance = -1 * (manhattanDistance(currentPos, x))
+            if (tempDistance > distance):
+                distance = tempDistance
+
+        return distance
